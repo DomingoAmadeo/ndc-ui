a8de2cd1feafa293590fcdec2f1febe28db369d1
Amadeo <domingofal@gmail.com>
2023-06-12 20:06:32 -0300

    Add update to pc

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Cobertura } from '../models/cobertura';
 import { Poliza } from '../models/poliza';
+import { PolizasCoberturasComponent } from '../polizas-coberturas/polizas-coberturas.component';
 
 @Component({
   selector: 'app-home',
@@ -8,6 +9,7 @@ import { Poliza } from '../models/poliza';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  @ViewChild('polizasCoberturas') polizasCoberturas: PolizasCoberturasComponent;
   polizas: Poliza[];
   coberturas: Cobertura[];
 
@@ -17,9 +19,11 @@ export class HomeComponent implements OnInit {
 
   updateCoberturas(coberturaArray: Cobertura[]){
     this.coberturas = coberturaArray;    
+    this.polizasCoberturas.updateTrigger$.next(0);
   }
 
   updatePoliza(polizaArray: Poliza[]){
     this.polizas = polizaArray;
+    this.polizasCoberturas.updateTrigger$.next(0);
   }
 }
